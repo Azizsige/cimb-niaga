80bb468bdd9fa80c7f49997ca7db81fa33892954
azizsige <kurniawan00azizsige@gmail.com>
2021-12-23 18:28:10 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    "./*.html",
+    "./images/*.png"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary": '#E8211D',
+        'secondary': '#C8CBD0',
+        'cardCalc': '#DEE1E6',
+        'cardInfo': '#B1B3B7',
+        'textAlternate': '#797979'
+      },
+      fontFamily: {
+        'primary': ['"Roboto", "sans-serif"']
+      }
+    },
+  },
+  plugins: [],
+}
